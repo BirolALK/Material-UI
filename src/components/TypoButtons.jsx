0b6f6952d71e2a1cb8372a7e39c0b915bc30b15b
Birol ALKAN <birolalkan93@gmail.com>
2022-11-10 19:40:41 +0300

    typo

--- src/components/TypoButtons.jsx
+++ src/components/TypoButtons.jsx
@@ -3,8 +3,12 @@ import Typography  from "@mui/material/Typography";
 const TypoButtons = () => {
   return (
     <div>
-      <Typography variant="h4" component="h6" align="center">
+      <Typography variant="h4" component="h6" align="center" mt={4} color="error">
         MUI Typography
+      </Typography> 
+
+      <Typography variant="body" align="justify" mt={4} sx={{background: "lightgreen", color: "#eee", fontSize: "1.2rem"}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet modi repellendus cupiditate cumque praesentium voluptatem recusandae. Impedit sunt odit et?
       </Typography> 
     </div>
   ) 
