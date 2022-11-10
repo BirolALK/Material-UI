8d3a62239a6a537868fefece4d1e10a575e62f9d
Birol ALKAN <birolalkan93@gmail.com>
2022-11-10 18:13:15 +0300

    first commit

---
+++ src/components/TypoButtons.jsx
@@ -0,0 +1,13 @@
+import Typography  from "@mui/material/Typography";
+
+const TypoButtons = () => {
+  return (
+    <div>
+      <Typography variant="h4" component="h6" align="center">
+        MUI Typography
+      </Typography> 
+    </div>
+  ) 
+};
+
+export default TypoButtons;
