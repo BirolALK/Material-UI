6e3f1f91f906705d17f0affc26d046d3a3cd4c92
Birol ALKAN <birolalkan93@gmail.com>
2022-11-10 23:42:40 +0300

    card

--- src/components/TextFieldComp.jsx
+++ src/components/TextFieldComp.jsx
@@ -3,8 +3,10 @@ import Container  from "@mui/material/Container";
 import  Button  from "@mui/material/Button";
 import Box  from "@mui/system/Box";
 import TextField  from "@mui/material/TextField";
+import { useState } from "react";
  
 const TextFieldComp = () => {
+    const[error, setError] = useState(false)
   return (
     <div>
         <Container>
@@ -12,9 +14,18 @@ const TextFieldComp = () => {
                 TEXT FIELD
             </Typography>
 
-            <Box>
+            <Box sx={{mt:4, textAlign: "center"}}>
                 <TextField id="email" label="Email"
-                placeholder="Enter your email"/>
+                placeholder="Enter your email"
+                fullWidth error={error} helperText={error && "Incorrect format"}/>
+                
+                <TextField margin="normal" id="password" label="Password"
+                placeholder="Enter your password"
+                fullWidth error={error} helperText={error && "Incorrect password"}/>
+                <Button variant="contained" color= "warning" sx={{mt:3}}>
+                    Submit
+                </Button>
+                
             </Box>
         </Container>
     </div>
